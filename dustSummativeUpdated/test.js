8ed8775eef4ec5169d05417a86bf709fc3cfbcbf
jayden-dunick <62521054+jayden-dunick@users.noreply.github.com>
2020-04-04 11:35:09 +1300

    This is my current site so far, still few bugs but about 90% of it's done

---
+++ dustSummativeUpdated/test.js
@@ -0,0 +1,54 @@
+console.log("Hello World!");
+
+//var left = document.getElementById('left');
+//var right = document.getElementById('right');
+//var image1 = document.getElementById('1');
+//var image2 = document.getElementById('2');
+//var image3 = document.getElementById('3');
+//
+//left.addEventListener('click', toggleLeft());
+//
+//function toggleLeft() {
+//    image1.classList.add("imageLeft");
+//}
+//
+//console.log("Hello World2!");
+
+
+const carouselSlide = document.querySelector('.carouselSlide');
+const carouselImages = document.querySelectorAll('.carouselSlide img');
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (carouselImages[counter].id === 'firstClone') {
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
